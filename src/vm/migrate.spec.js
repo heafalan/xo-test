6f28da7e4a11f94532a1a168bc7f6aed6ff8056b
badrAZ <azizbibadr@gmail.com>
2017-02-17 17:15:12 +0100

    update tests

--- src/vm/migrate.spec.js
+++ src/vm/migrate.spec.js
@@ -1,9 +1,6 @@
 /* eslint-env jest */
 
 import eventToPromise from 'event-to-promise'
-import {
-  map
-} from 'lodash'
 
 import {
   config,
@@ -13,51 +10,43 @@ import {
 
 // ===================================================================
 
-beforeEach(async () => {
-  jasmine.DEFAULT_TIMEOUT_INTERVAL = 40e3
+beforeAll(() => {
+  jasmine.DEFAULT_TIMEOUT_INTERVAL = 600e3
 })
 
+// ===================================================================
+
 describe('.migrate()', () => {
-  const vmsToDelete = []
-  let serverId
-  let vmId
+  describe('migrate the VM in the same pool', () => {
+    let vmId
 
-  // ----------------------------------------------------------------------
+    // ----------------------------------------------------------------------
 
-  beforeAll(async () => {
-    serverId = await xo.call('server.add', config.lab1)
-    await eventToPromise(xo.objects, 'finish')
-  })
+    afterAll(() => xo.call('vm.delete', {id: vmId, delete_disks: true}))
 
-  beforeEach(async () => {
-    vmId = await xo.call('vm.create', {
-      name_label: 'vmTest',
-      template: config.templatesId.centOS
-    })
-    vmsToDelete.push(vmId)
-    await waitObjectState(xo, vmId, vm => {
-      if (vm.type !== 'VM') throw new Error('retry')
-    })
+    // =================================================================
 
-    await xo.call('vm.start', {id: vmId})
-    await waitObjectState(xo, vmId, vm => {
-      if (vm.power_state !== 'Running') throw new Error('retry')
-    })
-  })
-
-  afterAll(async () => {
-    await Promise.all(map(vmsToDelete, id => xo.call('vm.delete', {id, delete_disks: true}).catch(error => console.error(error))))
-    vmsToDelete.length = 0
-
-    await xo.call('server.remove', {
-      id: serverId
-    })
-  })
-
-  // ----------------------------------------------------------------------
-
-  describe('migrate to an other host', () => {
     it('migrates the VM on an other host which is in the same pool', async () => {
+      vmId = await xo.call('vm.create', {
+        name_label: 'vmTest',
+        template: config.templatesId.debianCloud,
+        VIFs: [{network: config.labPoolNetworkId}],
+        VDIs: [{
+          device: '0',
+          size: 1,
+          SR: config.labPoolSrId,
+          type: 'user'
+        }]
+      })
+      await waitObjectState(xo, vmId, vm => {
+        if (vm.type !== 'VM') throw new Error('retry')
+      })
+
+      await xo.call('vm.start', {id: vmId})
+      await waitObjectState(xo, vmId, vm => {
+        if (vm.startTime === 0) throw new Error('retry')
+      })
+
       await xo.call('vm.migrate', {
         vm: vmId,
         targetHost: config.lab2Id
@@ -69,22 +58,48 @@ describe('.migrate()', () => {
     })
   })
 
-  // Received error: "VM_LACKS_FEATURE_SUSPEND"
-  describe.skip('migrate to an other pool', () => {
-    let secondServerId
+  describe('migrate the VM to an other pool', () => {
+    let serverId
+    let vmId
+
+    // ----------------------------------------------------------------------
 
     beforeAll(async () => {
-      secondServerId = await xo.call('server.add', config.lab4)
+      serverId = await xo.call('server.add', config.lab4)
       await eventToPromise(xo.objects, 'finish')
     })
 
     afterAll(async () => {
+      await xo.call('vm.delete', {id: vmId, delete_disks: true}).catch(error => console.error(error))
+
       await xo.call('server.remove', {
-        id: secondServerId
+        id: serverId
       })
     })
 
+    // =================================================================
+
     it('migrates the VM on an other host which is in an other pool', async () => {
+      vmId = await xo.call('vm.create', {
+        name_label: 'vmTest',
+        template: config.templatesId.debianCloud,
+        VIFs: [{network: config.labPoolNetworkId}],
+        VDIs: [{
+          device: '0',
+          size: 1,
+          SR: config.labPoolSrId,
+          type: 'user'
+        }]
+      })
+      await waitObjectState(xo, vmId, vm => {
+        if (vm.type !== 'VM') throw new Error('retry')
+      })
+
+      await xo.call('vm.start', {id: vmId})
+      await waitObjectState(xo, vmId, vm => {
+        if (vm.startTime === 0) throw new Error('retry')
+      })
+
       await xo.call('vm.migrate', {
         vm: vmId,
         targetHost: config.lab4Id
